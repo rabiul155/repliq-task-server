48ff7c45f12b34d0927926d4c26dc05f9c22c811
rabiul155 <siyamict@gmail.com>
2023-03-31 20:54:14 +0600

    db connect

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.i4cqwjk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', (req, res) => {
     res.send('server start')
